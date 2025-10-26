e7d18c31733f43058b814fee68b08586b2c30fb5
Mahmoud-Mokhtar <mahmoudmoukhtar93@gmail.com>
2025-10-26 09:57:32 +0300

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class IP
+    {
+        private int[] segments = new int[4];
+
+        // to make a class indexer must do the following
+        public int this[int index]
+        {
+            get
+            {
+                return segments[index];
+            }
+            set
+            {
+                segments[index] = value;
+            }
+        }
+        public IP(string ipaddresses)
+        {
+            var segs = ipaddresses.Split('.');
+            for (int i = 0; i < segs.Length; i++)
+            {
+                segments[i] = Convert.ToInt32(segs[i]);
+            }
+
+        }
+        public IP(int seg1,int seg2,int seg3 , int seg4) 
+        { 
+            segments[0] = seg1;
+            segments[1] = seg2;
+            segments[2] = seg3;
+            segments[3] = seg4;
+        }
+        public string address => string.Join('.', segments);
+    }
+
+    public class Soduko
+    {
+        private int[,] _matrix;
+        // public Soduko(int[,] matrix)
+        // {
+        //     this._matrix = matrix;
+        // }
+       
+       
+        public int this[int row,int col]
+        {
+            get
+            {
+                if(row < 0 || row > this._matrix.GetLength(0)-1) // getlength(0) mean rows
+                
+                    return -1;
+                
+                if (col < 0 || col > this._matrix.GetLength(1) - 1)  // getlength(1) mean columns
+                
+                    return -1;
+                
+                return this._matrix[row,col];
+            }
+            set
+            {
+                if (value < 0 || value > this._matrix.GetLength(0)-1)
+                
+                    return;
+
+                this._matrix[row,col] = value;
+            }
+            
+
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp7;
+//indexers
+Console.WriteLine("Hello, World!");
+var ip = new IP(122, 133, 12, 44);
+var ip1 = new IP("332.233.662.474");
+var ip2 = ip[2];
+Console.WriteLine($"Address : {ip.address}");
+Console.WriteLine($"Address : {ip1.address}");
+Console.WriteLine($"Address : {ip2}");
+
+// soduko array
+int[,] inputs = new int[,]{
+                 {8,3,5,4,1,6,9,2,7},
+                 {2,9,6,8,5,7,4,3,1},
+                 {4,1,7,2,9,3,6,5,8},
+                 {5,6,9,1,3,4,7,8,2},
+                 {1,2,3,6,7,8,5,4,9},
+                 {7,4,8,5,2,9,1,6,3},
+                 {6,5,2,7,8,1,3,9,4},
+                 {9,8,1,3,4,5,2,7,6},
+                 {3,7,4,9,6,2,8,1,5}
+               };
+
+
+Console.WriteLine(inputs[5, 5]);
+inputs[1, 1] = 4;
+Console.WriteLine(inputs[1, 1]);
+// var soduko = new Soduko(inputs);
+// Console.WriteLine(soduko[3,3]);
+// soduko[5, 5] = 6;
+// Console.WriteLine(soduko[5, 5]);
